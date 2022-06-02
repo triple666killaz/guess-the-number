ee4523c56ee1da94009a2f93b3cf8fbf190160d6
triple666killaz <blackrascal14@gmail.com>
2022-06-02 23:03:21 +0300

    First commit

---
+++ GuessGame.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GuessTheNumber
+{
+    class AppSettings
+    {
+        public int maxValue { get; set; }
+        public int minValue { get; set; }
+
+    }
+
+    public class GuessGame
+    {
+        private int hiddenNumber;
+        Random rand = new Random();
+        string jsonData;
+        private int randomMinValue;
+        private int randomMaxValue;
+
+        public GuessGame()
+        {
+            StreamReader reader = new StreamReader("..\\..\\..\\appSettings.json");
+            this.jsonData = reader.ReadToEnd();
+            AppSettings settings = JsonConvert.DeserializeObject<AppSettings>(jsonData);
+            this.randomMinValue = settings.minValue;
+            this.randomMaxValue = settings.maxValue + 1;
+            this.hiddenNumber = rand.Next(randomMinValue, randomMaxValue);
+        }
+
+        public GuessGame(int value)
+        {
+            this.hiddenNumber = value;
+        }
+
+        public bool AreYouWin(int value)
+        {
+            if (value == this.hiddenNumber)
+            {
+                this.hiddenNumber = rand.Next(randomMinValue, randomMaxValue);
+                return true;
+            }
+            return false;
+        }
+
+        public string GetHint(int value)
+        {
+            if (value < this.hiddenNumber)
+            {
+                return "Try to take a bigger number";
+            }
+            return "Try to take a smaller number";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void GameStarting()
+        {
+            Console.WriteLine("Hello!\nLet's start the game\nGame rules:\n1.Computer makes a number from 0 to 100(inclusive)\n2.You have to guess what number the computer thought\n3.If you guess right, you win, otherwise you get a hint");
+            Console.WriteLine("Good luck!");
+        }
+
+        static int GetNumber()
+        {
+            Console.WriteLine("Enter the number from 0 to 100(inclusive):");
+            string number = Console.ReadLine();
+            int result;
+            bool isConvert = int.TryParse(number, out result);
+            bool isError = !isConvert || result > 100 || result < 0;
+            while (isError)
+            {
+                Console.WriteLine("Number input error,try again!");
+                number = Console.ReadLine();
+                isConvert = int.TryParse(number, out result);
+                isError = !isConvert || result > 100 || result < 0;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            GuessGame game = new GuessGame();
+            bool stopButton = false;
+            GameStarting();
+            while (!stopButton)
+            {
+                int value = GetNumber();
+                if (game.AreYouWin(value))
+                {
+                    Console.WriteLine("Congratulations!!!You are win!");
+                    Console.WriteLine("Press 1 - to play again,0 - to exit");
+                    bool isConvert = int.TryParse(Console.ReadLine(), out value);
+                    bool isError = !isConvert || value > 1 || value < 0;
+                    while (isError)
+                    {
+                        Console.WriteLine("Input error,try again.Press 1 - to play again,0 - to exit");
+                        isConvert = int.TryParse(Console.ReadLine(), out value);
+                        isError = !isConvert || value > 1 || value < 0;
+                    }
+                    if (value == 0)
+                    {
+                        stopButton = true;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    GameStarting();
+                    Console.WriteLine("********** Game Zone ************");
+                    Console.WriteLine(game.GetHint(value));
+                }
+
+            }
+        }
+
+    }
+
+}
+
